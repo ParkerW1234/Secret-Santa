8e27aea9a5dc6cb60018ef132ebaa0bcde60026f
Parker Wasileski <parkerparker2468@gmail.com>
2026-01-21 20:33:09 -0500

    Update Set-Cookie header for logout function

--- netlify/functions/logout.js
+++ netlify/functions/logout.js
@@ -1,8 +1,10 @@
+// netlify/functions/logout.js
 export async function handler() {
   return {
     statusCode: 302,
     headers: {
-      "Set-Cookie": `session=; HttpOnly; Secure; Path=/; Max-Age=0`,
+      "Set-Cookie":
+        "session=; HttpOnly; Secure; SameSite=None; Path=/; Max-Age=0",
       Location: "/login.html"
     }
   };
